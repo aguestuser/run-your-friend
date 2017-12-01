be68d702f45ec2c7c43c5a98c88d834fe55b0a4a
aguestuser <aguestuser@riseup.net>
2017-12-01 16:12:30 -0500

    import splash page text from markdown file, edit text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
+import landingPageText from './text/landing-page.md';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Run your friend for office!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ReactMarkdown source={landingPageText} />
       </div>
     );
   }
---
+++ src/text/landing-page.md
@@ -0,0 +1,9 @@
+## Got a problem with government?
+
+You're not alone! [Fed up](http://news.gallup.com/poll/1600/congress-public.aspx) with their leaders, citizens are increasingly looking to each other to fix problems themselves by [running for office and winning](https://theintercept.com/2017/11/08/a-year-after-trump-democrats-socialists-and-populists-sweep-elections/).
+
+## Got a friend you trust?
+
+Use the form below to nominate them to run for office AND recruit their campaign team!
+
+If your friend receives more than 1,000 pledges of support, they will receive an email with a contact list of everyone who pledged their support to bootstrap their campaign.
